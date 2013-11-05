a191c5ea8f405cd6bf3941ad5502afa0d260c788
Michael Dotson <mdotson1@mdotson1-Aspire-7750G.(none)>
2013-11-05 10:15:01 -0600

    updated README, routes, and html

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,6 +9,14 @@ import views.html.*;
 
 public class Application extends Controller {
     public static Result index() {
-        return ok(index.render("Your new application is ready. DOOD"));
+        renderURLs();
+        return ok(view.render("students", routes.StudentResource.getAllStudents().absoluteURL(request())));
+    }
+
+    private static void renderURLs() {
+        view.render("students", routes.StudentResource.getAllStudents().absoluteURL(request()));
+        view.render("professors", routes.ProfessorResource.getAllProfessors().absoluteURL(request()));
+        view.render("courses", routes.CourseResource.getAllCourses().absoluteURL(request()));
+        view.render("semesters", routes.SemesterResource.getAllSemesters().absoluteURL(request()));
     }
 }
